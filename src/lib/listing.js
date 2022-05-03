9b0b26a8a6a177f06456e0420345f039dfb59194
Ranitea <ranitea.yongue.23@eigsi.fr>
2022-05-03 09:46:47 +0200

    Initial commit

---
+++ src/lib/listing.js
@@ -0,0 +1,36 @@
+const films = [
+	{
+		titre: 'Titanic',
+		date: '1997',
+		real: 'James Cameron',
+		duree: '194 min ',
+		genre: 'Drama, Romance',
+		imdbrating: '7,8',
+		pitch:
+			'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.'
+	},
+
+	{
+		titre: 'Aliens',
+		date: '1986',
+		real: 'James Cameron',
+		duree: '137 min ',
+		genre: 'Action, Adventure, Sci-Fi',
+		imdbrating: '8,3',
+		pitch:
+			'Fifty-seven years after surviving an apocalyptic attack aboard her space vessel by merciless space creatures, Officer Ripley awakens from hyper-sleep and tries to warn anyone who will listen about the predators.'
+	},
+
+	{
+		titre: 'Avatar',
+		date: '2009',
+		real: 'James Cameron',
+		duree: '162 min ',
+		genre: 'Action, Adventure, Fantasy',
+		imdbrating: '7,8',
+		pitch:
+			'A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.'
+	}
+];
+
+export default films;
---
+++ src/routes/index.svelte
@@ -0,0 +1,86 @@
+
+
+<script>
+	import films from '$lib/listing.js'
+	/*const priceTTC = (price) => price * 1.2*/
+
+</script>
+
+
+<section>
+	{#each films as film}
+		<div class="titre">{film.titre} <!--price : {priceTTC(product.price)}--></div>
+		<div class="infos"><div class="categ">Date de sortie:</div>{film.date} </div>
+		<div class="infos"><div class="categ">Réalisateur:</div>{film.real} </div>
+		<div class="infos"><div class="categ">Durée:</div>{film.duree} </div>
+		<div class="infos"><div class="categ">Genre:</div>{film.genre} </div>
+		<div class="infos"><div class="categ">Note Imdb:</div>{film.imdbrating} </div>
+		<div class="resume"><div class="categ">Résumé:</div>{film.pitch} </div>
+	{/each}
+</section>
+
+<!--
+<input id="searchbar" onkeyup="search_animal()" type="text" name="search" placeholder="Search animals..">-->
+      
+    <!-- ordered list 
+    <ol id='list'>
+        <li class="titre">Cat</li>
+        <li class="animals">Dog</li>
+        <li class="animals">Elephant</li>
+        <li class="animals">Fish</li>
+        <li class="animals">Gorilla</li>
+        <li class="animals">Monkey</li>
+        <li class="animals">Turtle</li>
+        <li class="animals">Whale</li>
+        <li class="animals">Aligator</li>
+        <li class="animals">Donkey</li>
+        <li class="animals">Horse</li>
+    </ol>
+
+		      
+     linking javascript 
+    <script src="./listing.js"></script>-->
+
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	.titre{
+		border: solid 0px black;
+		width: 100%;
+		color: #C60800;
+		font-weight: bold;
+		font-size: 30;
+		padding: 20px 50px 8px;
+		
+	}
+
+	.infos{
+		border: solid 0px black;
+		width: 100%;
+		padding: 8px 8px;
+		margin-left: 60%;
+	}
+
+	.resume{
+		border: solid 0px black;
+		width: 100%;
+		padding: 8px 8px;
+		margin-left: 0%;
+	}
+
+	.categ{
+		border: solid 0px black;
+		width: 100%;
+		font-weight: bold;
+		
+	}
+
+
+</style>
